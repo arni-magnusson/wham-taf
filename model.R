85631f1af7383f6051588d9cdd2d5d78751af6bb
Arni Magnusson <thisisarni@gmail.com>
2024-02-20 02:05:08 +1100

    Run model

--- model.R
+++ model.R
@@ -1,9 +1,20 @@
 ## Run analysis, write model results
 
-## Before:
-## After:
+## Before: input.rds (data)
+## After:  convergence.txt, model.rds (model)
 
 library(TAF)
+library(wham)
 
 mkdir("model")
 
+# Read input data
+input <- readRDS("data/input.rds")
+
+# Run model
+model <- fit_wham(input, do.osa=TRUE)
+convergence <- capture.output(check_convergence(model))
+
+# Write results
+saveRDS(model, "model/model.rds")
+write(convergence, "model/convergence.txt")
